27552723a81f94e082df3b5480c781113b8dc47a
Philippe Matray <phmatray@gmail.com>
2023-03-26 22:06:20 +0200

    Comments the SalesPitchCommand

--- src/SalesPitch/Commands/SalesPitchCommand.cs
+++ src/SalesPitch/Commands/SalesPitchCommand.cs
@@ -9,12 +9,22 @@ using Spectre.Console.Cli;
 
 namespace SalesPitch.Commands;
 
+/// <summary>
+///     This C# class defines a SalesPitchCommand class that represents a command-line interface (CLI)
+///     command for generating sales pitches. It uses OpenAI GPT-3 for generating the content and
+///     the Spectre.Console library for creating a user-friendly CLI.
+/// </summary>
 public sealed class SalesPitchCommand
     : AsyncCommand<SalesPitchSettings>
 {
     private readonly IOpenAIService _openAIService;
     private readonly SpectreLanguageServiceFactory _spectreLanguageServiceFactory;
     
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="SalesPitchCommand"/> class.
+    /// </summary>
+    /// <param name="openAIService">The OpenAI GPT-3 service that is used for generating the sales pitch.</param>
+    /// <param name="spectreLanguageServiceFactory">The Spectre.Console language service factory that is used for creating the language service.</param>
     public SalesPitchCommand(
         IOpenAIService openAIService,
         SpectreLanguageServiceFactory spectreLanguageServiceFactory)
@@ -23,8 +33,17 @@ public sealed class SalesPitchCommand
         _spectreLanguageServiceFactory = spectreLanguageServiceFactory;
     }
 
-    public override async Task<int> ExecuteAsync(CommandContext context, SalesPitchSettings settings)
+    /// <summary>
+    ///     Main method for executing the command asynchronously
+    /// </summary>
+    /// <param name="context">The command context</param>
+    /// <param name="settings">The command settings</param>
+    /// <returns>The exit code</returns>
+    public override async Task<int> ExecuteAsync(
+        CommandContext context,
+        SalesPitchSettings settings)
     {
+        // Display a header and a rule
         AnsiConsole.Write(
             new FigletText("SalesPitch")
                 .LeftJustified()
@@ -32,6 +51,7 @@ public sealed class SalesPitchCommand
 
         AnsiConsole.Write(new Rule());
 
+        // Prompt the user for various settings and store them
         settings.Language = AskLanguage();
         var languageService = _spectreLanguageServiceFactory
             .GetLanguageService(settings.Language);
@@ -39,7 +59,7 @@ public sealed class SalesPitchCommand
         settings.Framework = AskSalesPitchFramework(languageService);
         settings.IsDemo = AskIsDemo(languageService);
 
-        // set demo data if needed
+        // Set demo data if needed
         if (settings.IsDemo)
         {
             settings.Product = languageService.ProductDemo();
@@ -55,16 +75,18 @@ public sealed class SalesPitchCommand
             settings.Benefits = AskBenefitsIfMissing(settings.Benefits, languageService);
         }
         
-        // settings table
+        // Display a table with the settings
         AnsiConsole.WriteLine();
         var table = GetSettingsTable(settings, languageService);
         AnsiConsole.Write(table);
         AnsiConsole.WriteLine();
         
+        // Display a rule and a prompt
         AnsiConsole.Write(
             new Rule("[red]Prompt[/]").LeftJustified());
         AnsiConsole.WriteLine();
 
+        // Prepare a request for the OpenAI GPT-3 API
         ChatCompletionCreateRequest request = new()
         {
             Messages = new List<ChatMessage>
@@ -74,6 +96,7 @@ public sealed class SalesPitchCommand
             }
         };
         
+        // Send the request and process the response
         IAsyncEnumerable<ChatCompletionCreateResponse> completionResult = 
             _openAIService.ChatCompletion
                 .CreateCompletionAsStream(request, Models.ChatGpt3_5Turbo);
@@ -82,6 +105,7 @@ public sealed class SalesPitchCommand
         {
             if (completion.Successful)
             {
+                // Display the generated sales pitch character by character
                 AnsiConsole.Write(completion.GetContent());
             }
             else
@@ -99,6 +123,12 @@ public sealed class SalesPitchCommand
         return 0;
     }
 
+    /// <summary>
+    ///     Helper method to create a table with the current settings
+    /// </summary>
+    /// <param name="settings">The current settings</param>
+    /// <param name="languageService">The language service</param>
+    /// <returns>A table with the current settings</returns>
     private static Table GetSettingsTable(SalesPitchSettings settings, ILanguageService languageService)
     {
         var table = new Table();
@@ -152,6 +182,10 @@ public sealed class SalesPitchCommand
         return table;
     }
 
+    /// <summary>
+    ///     Methods for prompting the user to select or input Language
+    /// </summary>
+    /// <returns>The selected language</returns>
     private static SupportedLanguage AskLanguage()
     {
         // use reflection to get all the enum values for the SupportedLanguage
@@ -175,6 +209,11 @@ public sealed class SalesPitchCommand
         return language;
     }
 
+    /// <summary>
+    ///     Methods for prompting the user to select or input SalesPitchFramework
+    /// </summary>
+    /// <param name="languageService">The language service</param>
+    /// <returns>The selected SalesPitchFramework</returns>
     private static SalesPitchFramework AskSalesPitchFramework(ILanguageService languageService)
     {
         // use reflection to get all the enum values for the SalesPitchFramework
@@ -193,6 +232,11 @@ public sealed class SalesPitchCommand
         return salesPitchFramework;
     }
 
+    /// <summary>
+    ///     Method for prompting the user to select or input IsDemo flag
+    /// </summary>
+    /// <param name="languageService">The language service</param>
+    /// <returns>The selected IsDemo flag</returns>
     private static bool AskIsDemo(ILanguageService languageService)
     {
         // prompt for demo data
@@ -205,6 +249,12 @@ public sealed class SalesPitchCommand
         return isDemo;
     }
     
+    /// <summary>
+    ///     Method for prompting the user to select or input Product
+    /// </summary>
+    /// <param name="product">The current product</param>
+    /// <param name="languageService">The language service</param>
+    /// <returns>A product name as a string</returns>
     private static string AskProductIfMissing(string? product, ILanguageService languageService)
         => !string.IsNullOrWhiteSpace(product)
             ? product
@@ -214,6 +264,12 @@ public sealed class SalesPitchCommand
                         SurroundWithRed(languageService.ProductNotValid()))
                     .Validate(productToValidate => !string.IsNullOrWhiteSpace(productToValidate)));
 
+    /// <summary>
+    ///     Method for prompting the user to select or input Price
+    /// </summary>
+    /// <param name="price">The current price</param>
+    /// <param name="languageService">The language service</param>
+    /// <returns>A price as a string</returns>
     private static string AskPriceIfMissing(string? price, ILanguageService languageService)
         => !string.IsNullOrWhiteSpace(price)
             ? price
@@ -223,6 +279,12 @@ public sealed class SalesPitchCommand
                         SurroundWithRed(languageService.PriceNotValid()))
                     .Validate(priceToValidate => !string.IsNullOrWhiteSpace(priceToValidate)));
 
+    /// <summary>
+    ///     Method for prompting the user to select or input Features
+    /// </summary>
+    /// <param name="features">The features of the product</param>
+    /// <param name="languageService">The language service</param>
+    /// <returns>A list of features as a string</returns>
     private static string AskFeaturesIfMissing(string? features, ILanguageService languageService)
         => !string.IsNullOrWhiteSpace(features)
             ? features
@@ -232,18 +294,35 @@ public sealed class SalesPitchCommand
                         SurroundWithRed(languageService.FeaturesNotValid()))
                     .Validate(featuresToValidate => !string.IsNullOrWhiteSpace(featuresToValidate)));
 
-    private static string AskBenefitsIfMissing(string? settingsBenefits, ILanguageService languageService)
-        => !string.IsNullOrWhiteSpace(settingsBenefits)
-            ? settingsBenefits
+    /// <summary>
+    ///     Method for prompting the user to select or input Benefits of the product
+    /// </summary>
+    /// <param name="benefits">The benefits of the product</param>
+    /// <param name="languageService">The language service</param>
+    /// <returns>A list of benefits as a string</returns>
+    private static string AskBenefitsIfMissing(string? benefits, ILanguageService languageService)
+        => !string.IsNullOrWhiteSpace(benefits)
+            ? benefits
             : AnsiConsole.Prompt(
                 new TextPrompt<string>(languageService.BenefitsPrompt())
                     .InvalidChoiceMessage(
                         SurroundWithRed(languageService.BenefitsNotValid()))
                     .Validate(benefitsToValidate => !string.IsNullOrWhiteSpace(benefitsToValidate)));
     
+    /// <summary>
+    ///     Surrounds the text with the specified color
+    /// </summary>
+    /// <param name="text">The text to surround</param>
+    /// <param name="color">The color to surround the text with</param>
+    /// <returns>The text surrounded with the specified color</returns>
     private static string SurroundWithColor(string text, ConsoleColor color)
         => $"[{color}]{text}[/]";
     
+    /// <summary>
+    ///     Surrounds the text with red
+    /// </summary>
+    /// <param name="text">The text to surround</param>
+    /// <returns>The text surrounded with red</returns>
     private static string SurroundWithRed(string text)
         => SurroundWithColor(text, ConsoleColor.Red);
 }
